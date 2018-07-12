0d3cab8c42d370fb0a87f98cd571fbf2636df3c2
adem enes polat <ademenespolat@gmail.com>
2018-07-12 10:07:04 +0300

    ilk commit

---
+++ Liste Uygulama/jquery/js/script.js
@@ -0,0 +1,14 @@
+$("#res").load(function () {
+	alert("sdafs");
+});  
+
+
+$(document).ready(function () {
+	$("#para").fadeIn("slow")
+});
+
+
+
+$("#para").click(function() {
+	$("#para").hide();
+});
---
+++ Liste Uygulama/script.js
@@ -0,0 +1,21 @@
+$("#gec").click(function () {
+	$("#gec").slideUp(1000);
+	$("#gec2").slideDown(1000);
+});
+$("#gec2").click(function () {
+	$("#gec2").slideUp(1000);
+	$("#gec").slideDown(1000);
+});  
+/*$(document).ready(function () {
+	$(".close").click(function () {
+		$(".close").parent().animate({
+			//left: "0px",
+			opacity: "0",
+			height: '40px',
+		  	width: '0px',
+		});
+		
+		div.hide(0);
+		})
+});*/
+
